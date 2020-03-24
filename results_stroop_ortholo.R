99cc0f21573df6a83bc4a6fccfc77b5f96eefe1e
EricMenetre <33350889+ERPeric@users.noreply.github.com>
2020-03-24 13:32:16 +0100

    finished the power analyses, wait for Marina

--- results_stroop_ortholo.R
+++ results_stroop_ortholo.R
@@ -16,6 +16,9 @@ data_import(list_files = list_files)
 
 data_cleaning(data = data_ortholo)
 
+library(writexl)
+write_xlsx(data_ortholo, "data_ortholo_cleaned.xlsx")
+
 # Graphs and descriptive statistics
 
 data_ortholo%>%
@@ -41,8 +44,33 @@ summary(m0) # conditionincong   16.372      5.455 2174.256   3.001  0.00272
 
 # Power analysis
 
-mtest <- makeLmer(RT_clean ~ condition + (1|subject) + (1|repcor), data = data_ortholo, fixef=fixed, VarCorr=rand, sigma=res)
+VPC_subj <- 3209.8/(3209.8+985.7 + 16262.9)
+VPC_item <- 985.7/(3209.8+985.7 + 16262.9)
+VPC_ires <- 16262.9/(3209.8+985.7 + 16262.9)
                                                                                                 
+subject <- 1:13
+power <- powerSim(m0, nsim = 20)
+power
+pc1 <- powerCurve(m0, along = "order" , nsim = 50)
+plot(pc1)
 
-powerSim(m0, nsim = 20)
+temp <- data_ortholo%>%
+  group_by(subject)%>%
+  summarise()
 
+temp$subject_num <- 1:nrow(temp)
+
+data_ortholo <- data_ortholo%>%
+  left_join(temp, by = "subject")
+
+pc2 <- powerCurve(m0, along = "subject_num", nsim = 50)
+plot(pc2)
+
+# Extend the data in the model
+
+m2 <- extend(m0, along = "condition", n = 5000)
+power2 <- powerSim(m2)
+power2
+
+pc3 <- powerCurve(m2, along = "subject_num")
+plot(pc3)
